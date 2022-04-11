4edee2f9e8053dbd824cf520058be0d824a5d6f2
Espinita <jordyluzman@gmail.com>
2022-04-11 11:25:02 +0200

    CSS additions
    Added sone css for the form page

--- resources/views/post-teacher.blade.php
+++ resources/views/post-teacher.blade.php
@@ -12,31 +12,31 @@
         <form action="{{ url('post-teacher') }}" class="form-for-teacher" method="POST">
             @csrf
             <div class="divs-inside-form-teacher">
-                <label for="">Teacher Name</label>
+                <label id="label-firstname-in-form" for="">Teacher Name</label>
                 <input type="text" name="fName">
             </div>
             <div class="divs-inside-form-teacher">
-                <label for="">Teacher Last Name</label>
+                <label id="label-lastname-in-form" for="">Teacher Last Name</label>
                 <input type="text" name="lName">
             </div> 
             <div class="divs-inside-form-teacher">
-                <label for="gender">Gender</label>
-                <select name="gender">
+                <label id="label-gender-in-form" for="gender">Gender</label>
+                <select id="label-gender-select" name="gender">
                 <option value="F">Female</option>
                 <option value="M">Male</option>
                 </select>
             </div>
             <div class="divs-inside-form-teacher">
-                <label for="">Surname</label>
+                <label id="label-surname-in-form" for="">Surname</label>
                 <input type="text" name="sName">            
             </div>
             <div class="divs-inside-form-teacher">
                 <p>Origin of surname?</p>
                 <label for=""></label>
-                <textarea name="txtbxOrigin" type="text" rows="5" cols="40"></textarea>
+                <textarea id="txtarea-in-form" name="txtbxOrigin" type="text" rows="5" cols="40"></textarea>
             </div>
             <div class="divs-inside-form-teacher">
-            <label for="section">Section</label>
+            <label id="label-section-in-form" for="section">Section</label>
                 <select name="section">
                 @foreach($arrSections as $section)
                 <option value={{ $section['idSection'] }} > {{ $section['secName']}} </option>
